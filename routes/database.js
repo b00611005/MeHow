f61551ddc33bd1c7ec5fdb0f95afbaed1d108505
Mickie.Hsu <b00611005@ntu.edu.tw>
2017-09-13 11:33:02 +0800

    Fix bugs.

--- routes/database.js
+++ routes/database.js
@@ -12,8 +12,8 @@ var client = s3.createClient({
 	multipartUploadThreshold: 20971520, // this is the default (20 MB)
 	multipartUploadSize: 15728640, // this is the default (15 MB)
 	s3Options: {
-	  accessKeyId: AWS_ACCESS_KEY_ID,
-	  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+	  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 	  region: "us-west-1"
 	},
   });
@@ -43,7 +43,7 @@ exports.insertMemory = function(req, res){
 			localFile: response.data,
 		  
 			s3Params: {
-			  Bucket: S3_BUCKET_NAME,
+			  Bucket: process.env.S3_BUCKET_NAME,
 			  Key: imageURL
 			},
 		  };
